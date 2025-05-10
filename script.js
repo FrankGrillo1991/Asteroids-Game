e3f300e17547ece23043b1611c80ae71ddc3d1ad
FrankGrillo1991 <fsg12491@aol.com>
2025-05-10 01:07:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,39 @@ const ctx = canvas.getContext("2d");
 // Set canvas size
 canvas.width = 800;
 canvas.height = 600;
+
+// Player spaceship
+const player = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: 20,
+    angle: 0,
+    speed: 0,
+    rotateSpeed: 0.05,
+    moveSpeed: 0.1,
+    dx: 0,
+    dy: 0,
+    draw() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.beginPath();
+        ctx.moveTo(0, -this.radius);
+        ctx.lineTo(-this.radius, this.radius);
+        ctx.lineTo(this.radius, this.radius);
+        ctx.closePath();
+        ctx.fillStyle = "white";
+        ctx.fill();
+        ctx.restore();
+    },
+    update() {
+        this.x += this.dx;
+        this.y += this.dy;
+
+        // Keep the player within the canvas
+        if (this.x > canvas.width) this.x = 0;
+        if (this.x < 0) this.x = canvas.width;
+        if (this.y > canvas.height) this.y = 0;
+        if (this.y < 0) this.y = canvas.height;
+    }
+};
