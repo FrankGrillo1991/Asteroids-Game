d76349efec8d54522fa5b41a5f27f4070ac6da73
FrankGrillo1991 <fsg12491@aol.com>
2025-05-10 00:56:53 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the canvas elements and its context
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+// Set canvas size
+canvas.width = 800;
+canvas.height = 600;
